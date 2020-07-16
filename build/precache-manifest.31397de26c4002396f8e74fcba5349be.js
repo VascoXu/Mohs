beb39b5b0004f3e4b63cf32cca7079fcd7890504
Vasco Xu <vax1@pitt.edu>
2020-07-16 01:36:31 -0400

    Format for Heroku

---
+++ build/precache-manifest.31397de26c4002396f8e74fcba5349be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7ba876a893e024adf214189ed75afa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "464412fc9a0069d7bf1d",
+    "url": "/static/css/main.7a0c6f08.chunk.css"
+  },
+  {
+    "revision": "486f1acefb5ebd9f09e2",
+    "url": "/static/js/2.42cb74bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.42cb74bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464412fc9a0069d7bf1d",
+    "url": "/static/js/main.3999c355.chunk.js"
+  },
+  {
+    "revision": "6ff9e69566ece72c76e6",
+    "url": "/static/js/runtime-main.e595984c.js"
+  },
+  {
+    "revision": "17d6023a52e6655c82eb52d24883b51e",
+    "url": "/static/media/SmashLab.17d6023a.png"
+  }
+]);
